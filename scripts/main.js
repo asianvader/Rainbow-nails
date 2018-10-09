cc18c3912ddc20dcdb62c8d8f6cae7a874611c0a
asianvader <phoebe.voong@gmail.com>
2018-10-09 23:42:24 +0100

    add smooth scrolling

--- scripts/main.js
+++ scripts/main.js
@@ -4,4 +4,24 @@ $(document).ready(function () {
   $(".navbar-nav li a").click(function(event) {
     $(".navbar-collapse").collapse('hide');
   });
+
+  // Smooth scrolling
+  var $scrollLink = $('.scroll');
+  $scrollLink.click(function(e) {
+    e.preventDefault();
+    $('body,html').animate({
+      scrollTop: $(this.hash).offset().top - 66
+    }, 500 );
+  });
+
+  $(window).scroll(function() {
+    var scrollbarLocation = $(this).scrollTop();
+      $scrollLink.each(function() {
+      var sectionOffset = $(this.hash).offset().top - 20;
+        if ( sectionOffset <= scrollbarLocation ) {
+          $(this).parent().addClass('active');
+          $(this).parent().siblings().removeClass('active');
+      }
+    })
+  });
 });
